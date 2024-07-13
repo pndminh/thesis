9afe94baedf27022d7cfc48edd5d2bc4323fbe14
pndminh <miphanng@gmail.com>
2024-07-13 19:03:01 +0700

    refactor fetch caller input

--- frontend/pages/utils.py
+++ frontend/pages/utils.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import time
+import streamlit as st
 
 sys.path.append(",/")
 from backend.fetcher.fetcher import fetch_multiple_pages
@@ -23,34 +25,37 @@ def init_fetch_state(state):
         state.pagination = False
     if "max_pages" not in state:
         state.max_pages = 0
+    if "dynamic_fetch_options" not in state:
+        state.dynamic_fetch_options = {
+            "infinite_scroll": 0,
+            "expand_button_click": "",
+            "pagination": 0,
+        }
+
+
+def init_extract_state(state):
+    if "extract_method" not in state:
+        state.extract_method = [""]
 
 
 def clear_fetch_inputs(state):
     state["url_input"] = ""
     state.fetch_method = "Static fetch"
     state.scroll_timeout = 10
-    state.expand_button_text = ""
+    state.expand_button_click = ""
 
 
-async def fetch_url(state):
-    url = state.url_input
-    fetch_method = state.fetch_method
-    infinite_scroll = state.infinite_scroll
-    scroll_timeout = state.scroll_timeout
-    expand_button_click = state.expand_button_click
-    expand_button_text = state.expand_button_text
-    pagination = state.pagination
-    max_pages = state.max_pages
-
+async def fetch(url, fetch_method, dynamic_fetch_options):
+    # dynamic_fetch_options = {"infinite_scroll": 0, "expand_button_text": "", "pagination": 0}
     static_fetch = True if fetch_method == "Static fetch" else False
-    max_duration = scroll_timeout if infinite_scroll else 0
-    if expand_button_click:
-        expand_texts = expand_button_text.split(", ")
-        expand_texts = [expand_text.strip() for expand_text in expand_texts]
-    else:
-        expand_texts = None
-    pages = int(max_pages) if pagination else 0
+    infinite_scroll = False if dynamic_fetch_options["infinite_scroll"] == 0 else True
+    expand_button_click = (
+        False if dynamic_fetch_options["expand_button_click"] == "" else True
+    )
+    pagination = False if dynamic_fetch_options["pagination"] == 0 else True
 
+    expand_texts = dynamic_fetch_options["expand_button_click"].split(", ")
+    expand_texts = [expand_text.strip() for expand_text in expand_texts]
     url_list = url.split(",")
     url_list = [url.strip() for url in url_list]
     print(url_list)
@@ -58,11 +63,44 @@ async def fetch_url(state):
         html = await fetch_multiple_pages(
             url_list,
             static_fetch=static_fetch,
-            max_duration=max_duration,
             scroll=infinite_scroll,
+            max_duration=dynamic_fetch_options["infinite_scroll"],
             expand=expand_button_click,
             expand_button_text=expand_texts,
         )
     except Exception as e:
         logger.info("Error while fetching URL")
     return html
+
+
+def write_html_files(html_list):
+    for i, html in enumerate(html_list):
+        st.info("Writing file", icon="📝")
+        with open(f"results/html/html_{i}_{int(time.time())}.html", "w") as f:
+            f.write(html)
+
+
+def add_data(label, example_content, extract_methods, contents_to_extract: dict):
+    for extract_method in extract_methods:
+        extract_method = (
+            extract_method.replace(
+                "Select ",
+                "",
+            )
+            .lower()
+            .strip()
+            .replace(" ", "_")
+        )
+        # print(extract_method)
+
+    if label in contents_to_extract.keys():
+        print(
+            contents_to_extract[label][0],
+            type(contents_to_extract[label][0]),
+        )
+        contents_to_extract[label].append((example_content, extract_method))
+    else:
+        contents_to_extract[label] = []
+        contents_to_extract[label].append((example_content, extract_method))
+    print(contents_to_extract)
+    return contents_to_extract
